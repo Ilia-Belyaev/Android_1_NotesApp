e1ca266204484a90ac26c9450f7367ab8a01d048
Ilia Belyaev <ilya.belyaev.95@list.ru>
2021-06-03 18:42:51 +0300

    6 дз.пустышка

---
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.notesapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
+
